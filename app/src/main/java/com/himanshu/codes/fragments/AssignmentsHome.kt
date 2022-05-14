197a23c247332a058b90eeecf70b772cdd1a60d6
Him-anshuSharma <sharmanshu0103@gmail.com>
2022-05-14 13:10:50 +0530

    New commit

--- app/src/main/java/com/himanshu/codes/fragments/AssignmentsHome.kt
+++ app/src/main/java/com/himanshu/codes/fragments/AssignmentsHome.kt
@@ -76,7 +76,7 @@ class AssignmentsHome(private val UID: String) : Fragment() {
                     if (i.getAssignmentDeadline() > assignment.getAssignmentDeadline()) {
                         _assignments.add(count, assignment)
                         Log.d("ASSIGNMENT_ADD","RESULT_OK")
-                        Toast.makeText(context, count.toString(), Toast.LENGTH_SHORT).show()
+                        qqToast.makeText(context, count.toString(), Toast.LENGTH_SHORT).show()
                         break
                     } else {
                         count++
